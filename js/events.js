74e966e8acf7b5f2ec395d42817fd5012d9c93ce
Maxi Learner <sagmeister@gmail.com>
2019-04-23 21:50:20 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+ })
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $("img").on("load", function(e) {
+    $("img").addClass("tasty")
+  )}
+}
 
-// call functions here
+function pressIt() {
+  $(document).on("keydown", function(event) {
+    if (event.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
 
-});
+function submitIt() {
+  $("form").on("submit", function(e) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+$(document).ready(function() {
+
+// $("p").on("click", getIt())
+// $("img").on("load", frameIt())
+// $("typing").on("keydown", pressIt())
+// $("form").on("submit", submitIt())
+
+getIt()
+frameIt()
+pressIt()
+submitIt()
+})
